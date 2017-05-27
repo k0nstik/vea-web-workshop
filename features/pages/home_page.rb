da12e329aaaada9f71cf571a139075f471a235d2
Konstantin <konstik@gmail.com>
2017-05-27 20:19:43 +0300

    Git Test

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -1,10 +1,53 @@
 class HomePage < BasePage
     attr_accessor :button_start
   def initialize
+    @video_frame = Element.new(:xpath, '//div[@id = "video"]')
+    @try_now_button = Element.new(:xpath, '//button[@id = "start_button"]')
+    @signup_email = Element.new(:xpath, '//div[@id = "signup"]/descendant::input[@type = "email"]')
+    @signup_password1 = Element.new(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password1"]')
+    @signup_password2 = Element.new(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password2"]')
+    @signup_project_name = Element.new(:xpath, '//div[@id = "signup"]/descendant::input[@name = "project_name"]')
+    @signup_close_button = Element.new(:xpath, '//div[@id = "signup"]/descendant::img[@class = "closecross"]')
   end
 
   def visible?
+    @video_frame.visible?
+    @try_now_button.visible?
   end
+
+  def open_signup
+    @try_now_button.click
+  end
+
+  def signup_enter_email(email)
+    @signup_email.send_keys email
+  end
+  
+    def signup_enter_pass1(password1)
+    @signup_password1.send_keys password1
+  end
+  
+    def signup_enter_pass2(password2)
+    @signup_password2.send_keys password2
+  end
+
+     def signup_enter_passwords(password)
+      signup_enter_pass1 password
+      signup_enter_pass2 password
+  end
+  
+  def signup_enter_project_name(name)
+    @signup_project_name.send_keys name
+  end
+
+  def signup_close_signup()
+    @signup_close_button.click
+  end
+  
+
+  def login_enter_email(email)
+
+    end
 
   def load
     visit('/')
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,3 +1,33 @@
-When(/^I am on Appimation home page/) do
-  visit 'apimation.com'
+Given(/^I am on Appimation sign up page$/) do
+  # visit 'http://apimation.com'
+  # find(:xpath, '//button[@id = "signup-b"]').click
+  @tests.signup_tests.load_signup_page
+  # @pages.page_home.load
+  # @pages.page_home.visible?
+  # @pages.page_home.open_signup
+end
+
+When(/^I submit signup details$/) do
+  @tests.signup_tests.submit_signup_details
+  # visit 'http://apimation.com'
+  # find(:xpath, '//button[@id = "signup-b"]').click
+  # @pages.page_home.signup_enter_email 'test@venta.lv'
+  # @pages.page_home.signup_enter_passwords 'qwerty'
+  # @pages.page_home.signup_enter_project_name 'MyProject'
+  # sleep(2)
+  # @pages.page_home.signup_close_signup
+
+
+  # find(:xpath, '//div[@id = "signup"]/descendant::input[@type = "email"]').send_keys 'test@venta.lv'
+  # find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password1"]').send_keys 'qwerty'
+  # find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password2"]').send_keys 'qwerty'
+  # find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "project_name"]').send_keys 'MyProject'
+ 
+  # find(:xpath, '//div[@id = "signup"]/descendant::img[@class = "closecross"]').click
+
+  sleep(3)
+end
+
+When(/^I submit signup details without again password$/) do
+  @tests.signup_tests.submit_signup_details_no_again_password
 end
